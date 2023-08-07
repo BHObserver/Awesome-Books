e242f66536433ffffbd99ceb6ebc381a615dd5b4
BHObserver <BHObserver0@gmail.com>
2023-08-07 23:32:56 +0600

    Update main.js

--- main.js
+++ main.js
@@ -6,8 +6,12 @@ const book = {
 const add = document.querySelector('.add');
 add.addEventListener('click', () => {
   const title = document.querySelector('#title').value;
+  const displayTitle = document.querySelector('.title');
   const author = document.querySelector('#author').value;
+  const displayAuthor = document.querySelector('.author');
   book.title = title;
   book.author = author;
   library.push(book);
+  displayTitle.innerText = book.title;
+  displayAuthor.innerText = book.author;
 });
