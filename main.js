cdcab4879d4379a62cc1d6e6b9e040d79946f385
BHObserver <BHObserver0@gmail.com>
2023-08-08 16:09:37 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
-const library = [];
-const book = {
-  title: '',
-  author: '',
-};
 const add = document.querySelector('.add');
+let library = [];
+
+/* Adds a book in the library */
 add.addEventListener('click', () => {
+  const book = {
+    title: '',
+    author: '',
+  };
   const title = document.querySelector('#title').value;
-  const displayBooksContainer = document.querySelector('.display-books-container');
   const author = document.querySelector('#author').value;
+
+  const displayBooksContainer = document.querySelector('.display-books-container');
+
   const displayBook = document.createElement('div');
-  const displayTitle = document.createElement('h2');
-  const displayAuthor = document.createElement('h3');
-  const removeBtn = document.createElement('button');
-  const underline = document.createElement('hr');
-  underline.classList.add('underline');
   displayBook.classList.add('display-book');
+
+  const displayTitle = document.createElement('h2');
   displayBook.appendChild(displayTitle);
+
+  const displayAuthor = document.createElement('h3');
   displayBook.appendChild(displayAuthor);
+
+  const removeBtn = document.createElement('button');
   displayBook.appendChild(removeBtn);
-  displayBook.appendChild(underline);
-  displayBooksContainer.appendChild(displayBook);
   removeBtn.classList.add('remove');
   removeBtn.innerText = 'Remove';
+
+  const underline = document.createElement('hr');
+  underline.classList.add('underline');
+  displayBook.appendChild(underline);
+
+  displayBooksContainer.appendChild(displayBook);
+
   book.title = title;
   book.author = author;
   library.push(book);
   displayTitle.innerText = book.title;
   displayAuthor.innerText = book.author;
+
+  /* Removes a book from the library */
+  const remove = document.querySelectorAll('.remove');
+  remove.forEach((element) => {
+    element.addEventListener(('click'), () => {
+      const children = element.parentElement.childNodes;
+      const targetTitle = children[0].innerText;
+      const targetAuthor = children[1].innerText;
+      element.parentElement.remove();
+      library = library.filter((b) => b.title !== targetTitle || b.author !== targetAuthor);
+    });
+  });
 });
