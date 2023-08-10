b5fa2d7098edd2317adbf9cf44c9715d942ddb1f
Umu <hanny.ally123@gmail.com>
2023-08-10 16:30:50 +0300

    Fix linter=s

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
-class Library {
+class LibraryApp {
   constructor() {
     this.books = [];
-    this.loadBooks();
-  }
-
-  addBook(title, author) {
-    const book = {
-      title: title,
-      author: author,
-    };
-    this.books.push(book);
-    this.saveBooks();
-    return book;
-  }
-
-  removeBook(title, author) {
-    this.books = this.books.filter(
-      (book) => book.title !== title || book.author !== author
-    );
-    this.saveBooks();
-  }
-
-  saveBooks() {
-    localStorage.setItem("library", JSON.stringify(this.books));
-  }
-
-  loadBooks() {
-    const storedBooks = JSON.parse(localStorage.getItem("library")) || [];
-    this.books = storedBooks;
-  }
-}
-
-class UI {
-  constructor(library) {
-    this.library = library;
     this.displayBooksContainer = document.querySelector(
-      ".display-books-container"
+      '.display-books-container',
     );
-    this.titleInput = document.querySelector("#title");
-    this.authorInput = document.querySelector("#author");
-    this.addBtn = document.querySelector(".add");
+    this.titleInput = document.querySelector('#title');
+    this.authorInput = document.querySelector('#author');
+    this.addBtn = document.querySelector('.add');
 
-    this.addBtn.addEventListener("click", () => this.addBook());
+    this.addBtn.addEventListener('click', () => this.addBook());
+    this.loadBooks();
     this.displayBooks();
   }
 
@@ -49,56 +17,60 @@ class UI {
     const title = this.titleInput.value;
     const author = this.authorInput.value;
     if (title && author) {
-      const book = this.library.addBook(title, author);
+      const book = { title, author };
+      this.books.push(book);
+      this.saveBooks();
       this.displayBook(book);
       this.clearInputs();
     }
   }
 
   displayBooks() {
-    this.library.books.forEach((book) => this.displayBook(book));
+    this.books.forEach((book) => this.displayBook(book));
   }
 
   displayBook(book) {
-    const displayBook = document.createElement("div");
-    displayBook.classList.add("display-book");
+    const displayBook = document.createElement('div');
+    displayBook.classList.add('display-book');
 
-    const displayTitle = document.createElement("h2");
-    displayTitle.textContent = book.title;
-    displayBook.appendChild(displayTitle);
+    displayBook.innerHTML = `
+      <h2>${book.title}</h2>
+      <h3>${book.author}</h3>
+      <button class="remove">Remove</button>
+      <hr class="underline">
+    `;
 
-    const displayAuthor = document.createElement("h3");
-    displayAuthor.textContent = book.author;
-    displayBook.appendChild(displayAuthor);
-
-    const removeBtn = document.createElement("button");
-    removeBtn.classList.add("remove");
-    removeBtn.textContent = "Remove";
-    removeBtn.addEventListener("click", () => this.removeBook(book));
-    displayBook.appendChild(removeBtn);
-
-    const underline = document.createElement("hr");
-    underline.classList.add("underline");
-    displayBook.appendChild(underline);
-
+    const removeBtn = displayBook.querySelector('.remove');
+    removeBtn.addEventListener('click', () => this.removeBook(book));
     this.displayBooksContainer.appendChild(displayBook);
   }
 
   removeBook(book) {
-    this.library.removeBook(book.title, book.author);
+    this.books = this.books.filter(
+      (b) => b.title !== book.title || b.author !== book.author,
+    );
+    this.saveBooks();
     this.clearDisplay();
     this.displayBooks();
   }
 
+  saveBooks() {
+    localStorage.setItem('library', JSON.stringify(this.books));
+  }
+
+  loadBooks() {
+    const storedBooks = JSON.parse(localStorage.getItem('library')) || [];
+    this.books = storedBooks;
+  }
+
   clearInputs() {
-    this.titleInput.value = "";
-    this.authorInput.value = "";
+    this.titleInput.value = '';
+    this.authorInput.value = '';
   }
 
   clearDisplay() {
-    this.displayBooksContainer.innerHTML = "";
+    this.displayBooksContainer.innerHTML = '';
   }
 }
 
-const library = new Library();
-const ui = new UI(library);
+const libraryApp = new LibraryApp();
